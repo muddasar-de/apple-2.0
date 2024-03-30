46d459bc1649ba46e2774e1bd87e2de6d961c920
muddasar-de <muddasarajmal87@gmail.com>
2024-03-31 00:48:11 +0500

    Apple 2.0 With sentry service implementaion.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import { appleImg, searchImg, bagImg } from '../utils';
+import { navLists } from './../constants/index';
+const Navbar = () => {
+  return (
+    <header className='w-full py-5 px-5 sm:px-10 flex justify-between items-center'>
+      <nav className='flex w-full screen-max-width'>
+        <div className='flex gap-2 justify-center items-center'>
+          <img src={appleImg} alt='Apple' width={14} height={18} />
+          <p>Apple 2.0</p>
+        </div>
+        <div className='flex flex-1 justify-center max-sm:hidden'>
+          {navLists.map((nav) => {
+            return (
+              <div
+                key={nav}
+                className='px-5 cursor-pointer text-sm text-gray hover:text-white transition-all'>
+                {nav}
+              </div>
+            );
+          })}
+        </div>
+        <div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
+          <img src={searchImg} alt='search' width={18} height={18} />
+          <img src={bagImg} alt='bag' width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Highlights from './Highlights';
+import VideoCarousel from './VideoCarousel';
+export { Navbar, Hero, Highlights, VideoCarousel };
